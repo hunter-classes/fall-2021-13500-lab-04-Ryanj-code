b9c67da5e99d7ed0f0f2ca6a8dd4db02bc7369ad
Ryanj-code <83621881+Ryanj-code@users.noreply.github.com>
2021-09-26 15:46:18 -0400

    Update checkerboard.cpp

--- checkerboard.cpp
+++ checkerboard.cpp
@@ -34,25 +34,30 @@ std::string cboard3x3(int w, int h){
   int stars = 0;
   int spaces = 0;
   
-  for(int i = 0; i < h; i++){
+  
     for(int j = 0; j < w; j++){
-      if(stars < 3){
-	s += "*";
-	stars++;
+      if((i % 6 < 3 && j % 6 < 3) || (i % 6 >= 3 && j % 6 >= 3)){
+        // Only adds * to s if the rows and columns(i and j) will are in the correct position(the if condition) for an * to be placed.  
+        if(stars < 3){
+	        s += "*";
+          stars++;
+        } // Adds * if stars is less than 3 and increments stars.
+        if(stars == 3){
+	        stars = 0;
+        } // Resets stars if it == 3.
       }
-      if(space < 3){
-	s += " ";
-	space++;
-      }
-      if(stars == 3){
-	stars = 0;
-      }
-      if(space == 3){
-	space = 0;
+      else{ // If position is not for an *, add spaces to s.
+        if(spaces < 3){
+	        s += " ";
+	        spaces++;
+        } // Adds spaces if spaces is less than 3 and increments stars. 
+        if(spaces == 3){
+	        spaces = 0;
+        } // Resets spaces if it == 3.
       }
     }
     s += "\n";
-  }
+  } 
 
   return s;
 }
